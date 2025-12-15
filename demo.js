b1cdbb457918085d8303a77139ffdcde0002d207
Srinidhi <srinidhihv1@gmail.com>
2025-12-15 14:58:24 +0530

    msg came

---
+++ demo.js
@@ -0,0 +1,34 @@
+// Variables
+let name = "Srinidhi";
+let age = 22;
+
+// Output
+console.log("Name:", name);
+console.log("Age:", age);
+
+// Function
+function greet(user) {
+    return "Hello, " + user;
+}
+
+console.log(greet(name));
+
+// Array
+let numbers = [1, 2, 3, 4, 5];
+
+// Loop
+for (let num of numbers) {
+    console.log(num);
+}
+
+// Object
+let user = {
+    username: "srinidhi",
+    role: "developer"
+};
+
+console.log(user.username);
+
+// Arrow function
+const add = (a, b) => a + b;
+console.log(add(10, 20));
